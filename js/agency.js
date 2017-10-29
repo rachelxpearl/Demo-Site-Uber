f8a3bd61abb85c5d740dc987cfd6448374733bd5
Rachel Turgeon <rpturgeo@syr.edu>
2017-10-29 15:10:18 -0400

    10/29

--- js/agency.js
+++ js/agency.js
@@ -35,4 +35,15 @@
     }
   });
 
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108820803-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108820803-1');
+</script>
+
+
 })(jQuery); // End of use strict
